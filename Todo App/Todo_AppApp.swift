03d5025f2178e0d363dd0fe06a60cadd2b0532a5
Dana <danaalfanek123@gmail.com>
2023-08-03 16:09:48 +0300

    Initial Commit

---
+++ Todo App/Todo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_AppApp.swift
+//  Todo App
+//
+//  Created by Dana Al Fanek on 03/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Todo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

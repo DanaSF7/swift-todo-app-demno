78962c7157c490cfab609a15250c172fd3f0e16c
Dana <danaalfanek123@gmail.com>
2023-08-03 17:24:51 +0300

    i added a subtitle

--- Todo App/ContentView.swift
+++ Todo App/ContentView.swift
@@ -11,10 +11,10 @@ struct ContentView: View {
     
     @State private var todos = [
         Todo(title: "Feed the cat"),
-        Todo(title: "Buy groceries"),
+        Todo(title: "Buy groceries", subtitle: "Bananas,eggs,milk,potatoes"),
         Todo(title: "Finish project"),
         Todo(title: "Call mom", isCompleted: true),
-        Todo(title: "Go for a run"),
+        Todo(title: "Go for a run",subtitle: "2km"),
         Todo (title: "Read a book")
     ]
     var body: some View {
@@ -25,17 +25,26 @@ struct ContentView: View {
                         .onTapGesture {
                             todo.isCompleted.toggle()
                         }
-                    Text(todo.title)
-                        .strikethrough(todo.isCompleted)
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text(todo.title)
+                            .strikethrough(todo.isCompleted)
+                        if !todo.subtitle.isEmpty{
+                            Text(todo.subtitle)
+                                .font(.caption)
+                                .foregroundColor(.gray)
+                                .strikethrough(todo.isCompleted)
+                            
+                        }
+                    }
                 }
+                .navigationTitle("Todos")
             }
-            .navigationTitle("Todos")
         }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }

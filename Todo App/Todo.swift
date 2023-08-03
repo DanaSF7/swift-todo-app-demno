17c0bddf4b2fcca6c7bacb469f798795ed07eafa
Dana <danaalfanek123@gmail.com>
2023-08-03 16:36:14 +0300

    added to do model and showed it on content view with a list

---
+++ Todo App/Todo.swift
@@ -0,0 +1,16 @@
+//
+//  Todo.swift
+//  Todo App
+//
+//  Created by Dana Al Fanek on 03/08/2023.
+//
+
+import Foundation
+
+struct Todo: Identifiable {
+    
+    var id = UUID()
+    var title: String
+    var isCompleted = false
+    
+}
